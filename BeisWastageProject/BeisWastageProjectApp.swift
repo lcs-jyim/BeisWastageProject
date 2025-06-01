0415dda3adb8190d9188a43634378ceff66cf2a7
Josh Yim <jyim@lcs.on.ca>
2025-05-31 21:29:34 -0400

    Initial Commit

---
+++ BeisWastageProject/BeisWastageProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeisWastageProjectApp.swift
+//  BeisWastageProject
+//
+//  Created by junxi Yim on 2025/5/31.
+//
+
+import SwiftUI
+
+@main
+struct BeisWastageProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeisWastageProjectTests/BeisWastageProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeisWastageProjectTests.swift
+//  BeisWastageProjectTests
+//
+//  Created by junxi Yim on 2025/5/31.
+//
+
+import Testing
+@testable import BeisWastageProject
+
+struct BeisWastageProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
